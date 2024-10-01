e7e9121bd4c537d39357447821e3f656f1c10ce0
Hilal Akçakaya <77569261+hilalkcky@users.noreply.github.com>
2024-10-01 16:16:53 +0300

    Update danısmanlık.py

--- danısmanlık.py
+++ danısmanlık.py
@@ -6,9 +6,12 @@ from streamlit_navigation_bar import st_navbar
 st.set_page_config(page_title="KAAN DANIŞMANLIK", initial_sidebar_state="collapsed")
 st.markdown("""
 <style>
-    * {
-       overflow-anchor: none !important;
-       }
+    .css-1kyxreq {  # Bu sınıfı uygulamanıza göre özelleştirin
+        max-width: 600px;  /* Örnek genişlik ayarı */
+        margin: auto;  /* Merkeze yerleştirme */
+    }
+</style>
+""", unsafe_allow_html=True)
 </style>""", unsafe_allow_html=True)
 html_string = """
   .stApp > header {
