9c31aed8d02df46c0717f57e250254caaae95412
Hilal Akçakaya <77569261+hilalkcky@users.noreply.github.com>
2024-10-03 13:31:59 +0300

    Update danısmanlık.py

--- danısmanlık.py
+++ danısmanlık.py
@@ -11,16 +11,21 @@ st.markdown("""
     }
 </style>
 """, unsafe_allow_html=True)
+
 html_string = """
   .stApp > header {
       background-color: transparent;
   }
 
   .stApp {
-    
+      margin: auto;
+      font-family: "Gill Sans Extrabold", sans-serif;
+      overflow: auto;
 
       background: black;
-      
+      animation: gradient 15s ease infinite;
+      background-size: 400% 400%;
+      background-attachment: fixed;
   }
   """
 styles = {
@@ -54,94 +59,6 @@ page = st_navbar(
 )
 
 
-# Ana Sayfa
 
 
-
-
-if page == "Ana Sayfa":
-    st.markdown(
-        """
-        <style>
-        h1, h2, h3, h4, h5, h6, p {
-            color: white;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-    # Başlık
-    st.title("Kaan Danışmanlık")
-
-    st.subheader("Merhabalar ülkemizin değerli misafirleri,")
-    st.write("""
-    Öncelikle hepinizi sevgi ile kucaklıyor, ülkenizden ayrılmanın verdiği hüznü sizlerle paylaşmak istiyoruz.
-    Kaan Danışmanlık olarak uzman kadromuzla birlikte 8 yıldır sizden önceki misafirlerimize kaliteli hizmet sunduk ve bu kalitemizi sizlere de hizmet vererek devam ettirmek istiyoruz.
-    Peki biz 8 yıldır neler mi yapıyoruz?
-    """)
-
-    # Kullanıcı Yorumları
-    st.subheader("Müşteri Yorumları")
-
-    # Yorum 1
-    
-    st.write("Phillip Mayer, 35")
-    st.write("""
-    "Kaan Danışmanlık ile göçmenlik süreci hakkında endişelenmenize gerek yok. Kendimi hoş ve emin ellerde hissettim."
-    """)
-
-    # Yorum 2
-
-    st.write("Noni Faraji, 25")
-    st.write("""
-    "Türkiye'ye tek başıma okumak için gelmek çok stresli olabilirdi. Neyse ki, Kaan Danışmanlık benim evden uzaktaki ailemdi ve her şeyi halletmeme yardım etti."
-    """)
-
-# Hizmetlerimiz sayfası
-elif page == "Hizmetlerimiz":
-    st.markdown(
-        """
-        <style>
-        h1, h2, h3, h4, h5, h6, p {
-            color: white;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-    st.title("Hizmetlerimiz")
-    st.subheader("""Kaan Danışmanlık olarak sunduğumuz hizmetler:""")
-   
-       
-
-# İletişim sayfası
-elif page == "İletişim":
-    st.markdown(
-        """
-        <style>
-        h1, h2, h3, h4, h5, h6, p {
-            color: white;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-    st.title("İletişim")
-    st.write("""
-    Bize ulaşmak için aşağıdaki bilgileri kullanabilirsiniz:
-    """)
-
-    # Adres
-    st.subheader("Adres")
-    st.subheader(
-        "[Cumhuriyet Mahallesi Tuna Caddesi No: 11/18 Çanakçı İşhanı Çankaya/Ankara](https://www.google.com/maps/search/?api=1&query=Cumhuriyet+Mahallesi+Tuna+Caddesi+No:+11/18+Çanakçı+İşhanı+Çankaya/Ankara)")
-    st.write("#")
-    st.write("Diğer şubemiz:")
-    st.write(
-        "[Osmanağa Mah. Halitağa Cad. No:31 Daire:6 Kadıköy İstanbul](https://www.google.com/maps/search/?api=1&query=Osmanağa+Mah.+Halitağa+Cad.+No:31+Daire:6+Kadıköy+İstanbul)")
-
-    # Telefon numarası
-    st.subheader("Telefon: *0553 794 6771*")
-
-    # Logo için alan
 
