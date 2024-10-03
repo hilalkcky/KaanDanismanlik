3c88c9d6aa11223d67f6f21274b27222ef05e804
Hilal Akçakaya <77569261+hilalkcky@users.noreply.github.com>
2024-10-03 12:59:15 +0300

    Update danısmanlık.py

--- danısmanlık.py
+++ danısmanlık.py
@@ -6,21 +6,38 @@ from streamlit_navigation_bar import st_navbar
 st.set_page_config(page_title="KAAN DANIŞMANLIK", initial_sidebar_state="collapsed")
 
 
-st.write("deneme")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("#")
-st.write("deneme")
+html_string = """
+  .stApp > header {
+      background-color: transparent;
+  }
+
+  .stApp {
+      margin: auto;
+      font-family: "Gill Sans Extrabold", sans-serif;
+      overflow: auto;
+
+      background: black;
+      animation: gradient 15s ease infinite;
+      background-size: 400% 400%;
+      background-attachment: fixed;
+  }
+  """
+styles = {
+    "nav": {
+        "background-color": "black",
+    },
+
+    "span": {
+
+        "color": "white",
+
+        "margin": "auto",
+        "font-family": "'Gill Sans Extrabold', sans-serif",
+        "overflow": "auto"
+    },
+
+    "hover": {
+        "background-color": " #AEEEEE",
+    },
+}
+st.markdown(f'<style>{html_string}</style>', unsafe_allow_html=True)
