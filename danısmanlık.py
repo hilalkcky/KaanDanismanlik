d8d01cbc2e067263536c2b91b44fcf8d9521954d
Hilal Akçakaya <77569261+hilalkcky@users.noreply.github.com>
2024-10-03 14:06:43 +0300

    Update danısmanlık.py

--- danısmanlık.py
+++ danısmanlık.py
@@ -4,14 +4,18 @@ import pydeck as pdk
 st.set_page_config(page_title="KAAN DANIŞMANLIK", initial_sidebar_state="collapsed")
 st.markdown("""
 <style>
-    .css-1kyxreq {  # Bu sınıfı uygulamanıza göre özelleştirin
-        max-width: 600px;  /* Örnek genişlik ayarı */
-        margin: auto;  /* Merkeze yerleştirme */
+    .css-1kyxreq {  
+        max-width: 600px;  
+        margin: auto;  
     }
     .radio-container {
         display: flex;
         justify-content: center;  /* Ortalamak için */
         margin-top: 20px;  /* Üstten boşluk */
+    }
+    .radio-label {
+        margin: 0 15px;  /* Her bir butonun yanındaki boşluk */
+        color: white;  /* Yazı rengi beyaz */
     }
 </style>
 """, unsafe_allow_html=True)
@@ -35,7 +39,7 @@ html_string = """
 st.markdown(f'<style>{html_string}</style>', unsafe_allow_html=True)
 
 # Sayfa seçim
-page = st.radio("", ["Ana Sayfa", "Hizmetlerimiz", "İletişim"], horizontal=True, key="menu_radio", help="Sayfa seçin")
+page = st.radio("", ["Ana Sayfa", "Hizmetlerimiz", "İletişim"], index=0, key="menu_radio", format_func=lambda x: x, help="Sayfa seçin")
 
 st.write("#")
 # Ana Sayfa
@@ -82,5 +86,95 @@ if page == "Ana Sayfa":
     """)
 
     # Yorum 2
-    st.image("noni.jpeg", width=500) 
+    st.image("noni.jpeg", width=500)  # Yorum sahibinin fotoğrafı
+    st.write("Noni Faraji, 25")
+    st.write("""
+    "Türkiye'ye tek başıma okumak için gelmek çok stresli olabilirdi. Neyse ki, Kaan Danışmanlık benim evden uzaktaki ailemdi ve her şeyi halletmeme yardım etti."
+    """)
+
+# Hizmetlerimiz sayfası
+elif page == "Hizmetlerimiz":
+    st.markdown(
+        """
+        <style>
+        h1, h2, h3, h4, h5, h6, p {
+            color: white;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    st.title("Hizmetlerimiz")
+    st.subheader("""Kaan Danışmanlık olarak sunduğumuz hizmetler:""")
+    services = [
+        ("Çalışma İzni", "WhatsApp Image 2024-10-01 at 13.56.24 (2).jpeg"),
+        ("Oturum İzni", "WhatsApp Image 2024-10-01 at 13.56.24 (3).jpeg"),
+        ("Deport İşlemleri", "deport.jpeg"),
+        ("Yabancı Hukuk İşlemleri", "WhatsApp Image 2024-10-01 at 13.56.24.jpeg"),
+        ("Evlilik İşlemleri", "WhatsApp Image 2024-10-01 at 13.56.24 (4).jpeg"),
+        ("Öğrenci İşlemleri", "ogrenci.jpeg"),
+        ("Yabancı Sağlık Sigortası", "WhatsApp Image 2024-10-01 at 13.56.24 (1).jpeg"),
+        ("Randevu İşlemleri", "son.jpeg"),
+    ]
+
+    for service, image in services:
+        st.markdown(f"<h3 style='font-size: 24px; font-weight: bold; color: white;'>{service}</h3>",
+                    unsafe_allow_html=True)
+        st.image(image, width=700)
+
+# İletişim sayfası
+elif page == "İletişim":
+    st.markdown(
+        """
+        <style>
+        h1, h2, h3, h4, h5, h6, p {
+            color: white;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    st.title("İletişim")
+    st.write("""Bize ulaşmak için aşağıdaki bilgileri kullanabilirsiniz:""")
+
+    # Adres
+    st.subheader("Adres")
+    st.subheader(
+        "[Cumhuriyet Mahallesi Tuna Caddesi No: 11/18 Çanakçı İşhanı Çankaya/Ankara](https://www.google.com/maps/search/?api=1&query=Cumhuriyet+Mahallesi+Tuna+Caddesi+No:+11/18+Çanakçı+İşhanı+Çankaya/Ankara)")
+    st.write("#")
+    st.write("Diğer şubemiz:")
+    st.write(
+        "[Osmanağa Mah. Halitağa Cad. No:31 Daire:6 Kadıköy İstanbul](https://www.google.com/maps/search/?api=1&query=Osmanağa+Mah.+Halitağa+Cad.+No:31+Daire:6+Kadıköy+İstanbul)")
+
+    # Telefon numarası
+    st.subheader("Telefon: *0553 794 6771*")
+
+    # Logo için alan
+    st.image("logo.jpeg", width=500)
+
+    latitude = 39.923952513749335
+    longitude = 32.85656321135337
+    view_state = pdk.ViewState(
+        latitude=latitude,
+        longitude=longitude,
+        zoom=11,  # Yakınlaştırma seviyesi
+        pitch=50  # Haritanın eğim açısı
+    )
+
+    layer = pdk.Layer(
+        'ScatterplotLayer',
+        data=[{'lat': latitude, 'lon': longitude}],
+        get_position='[lon, lat]',
+        get_radius=500,  # İşaretçinin büyüklüğü
+        get_color=[255, 0, 0],  # Kırmızı renk
+        pickable=True
+    )
+
+    deck = pdk.Deck(
+        layers=[layer],
+        initial_view_state=view_state,
+        map_style="mapbox://styles/mapbox/streets-v11"  # Google Haritalar benzeri bir stil
+    )
+
+    st.pydeck_chart(deck)
 
