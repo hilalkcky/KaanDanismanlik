b7998278cc094e9ccfddf39ccbe1ff2b7621dd03
Hilal Akçakaya <77569261+hilalkcky@users.noreply.github.com>
2024-10-03 13:26:53 +0300

    Update danısmanlık.py

--- danısmanlık.py
+++ danısmanlık.py
@@ -3,7 +3,14 @@ import pydeck as pdk
 from streamlit_navigation_bar import st_navbar
 
 st.set_page_config(page_title="KAAN DANIŞMANLIK", initial_sidebar_state="collapsed")
-
+st.markdown("""
+<style>
+    .css-1kyxreq {  # Bu sınıfı uygulamanıza göre özelleştirin
+        max-width: 600px;  /* Örnek genişlik ayarı */
+        margin: auto;  /* Merkeze yerleştirme */
+    }
+</style>
+""", unsafe_allow_html=True)
 html_string = """
   .stApp > header {
       background-color: transparent;
