6c8eb85d528d3867981388f4d6d087231049f4d3
Hilal Akçakaya <77569261+hilalkcky@users.noreply.github.com>
2024-10-03 12:57:10 +0300

    Update danısmanlık.py

--- danısmanlık.py
+++ danısmanlık.py
@@ -4,59 +4,7 @@ from streamlit_navigation_bar import st_navbar
 
 
 st.set_page_config(page_title="KAAN DANIŞMANLIK", initial_sidebar_state="collapsed")
-st.markdown("""
-<style>
-    .css-1kyxreq {  # Bu sınıfı uygulamanıza göre özelleştirin
-        max-width: 600px;  /* Örnek genişlik ayarı */
-        margin: auto;  /* Merkeze yerleştirme */
-    }
-</style>
-""", unsafe_allow_html=True)
 
-html_string = """
-  .stApp > header {
-      background-color: transparent;
-  }
-
-  .stApp {
-      margin: auto;
-      font-family: "Gill Sans Extrabold", sans-serif;
-      overflow: auto;
-
-      background: black;
-      background-size: 400% 400%;
-      background-attachment: fixed;
-  }
-  """
-styles = {
-    "nav": {
-        "background-color": "black",
-    },
-
-    "span": {
-
-        "color": "white",
-
-        "margin": "auto",
-        "font-family": "'Gill Sans Extrabold', sans-serif",
-        "overflow": "auto"
-    },
-
-    "hover": {
-        "background-color": " #AEEEEE",
-    },
-}
-st.markdown(f'<style>{html_string}</style>', unsafe_allow_html=True)
-
-# Sidebar menüsü
-st.sidebar.title("Menü")
-pages = ["Ana Sayfa", "Hizmetlerimiz", "İletişim"]
-options = {"show_sidebar": False}
-page = st_navbar(
-    pages,
-    styles=styles,
-    options=options,
-)
 
 st.write("deneme")
 st.write("#")
@@ -67,5 +15,12 @@ st.write("#")
 st.write("#")
 st.write("#")
 st.write("#")
-
+st.write("#")
+st.write("#")
+st.write("#")
+st.write("#")
+st.write("#")
+st.write("#")
+st.write("#")
+st.write("#")
 st.write("deneme")
