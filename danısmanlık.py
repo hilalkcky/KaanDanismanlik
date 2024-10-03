ebece155f036dd0f841c2699f49c604085a97cbe
Hilal Akçakaya <77569261+hilalkcky@users.noreply.github.com>
2024-10-03 13:05:40 +0300

    Update danısmanlık.py

--- danısmanlık.py
+++ danısmanlık.py
@@ -42,6 +42,67 @@ styles = {
 }
 st.markdown(f'<style>{html_string}</style>', unsafe_allow_html=True)
 
+# Sidebar menüsü
+st.sidebar.title("Menü")
+pages = ["Ana Sayfa", "Hizmetlerimiz", "İletişim"]
+
+page = st_navbar(
+    pages,
+    styles=styles,
+
+)
+
+st.write("#")
+# Ana Sayfa
+st.markdown(
+    """
+    <h2 style="background-color: #AEEEEE; font-weight: bold; padding: 10px; border-radius: 5px; color: black;">
+        TÜRKİYE'YE GELDİNİZ VE NE YAPACAĞINIZI BİLEMİYOR MUSUNUZ? YENİ HAYATINIZA KAAN DANIŞMANLIĞI ZİYARET EDEREK BAŞLAYABİLİRSİNİZ.
+    </h2>
+    """,
+    unsafe_allow_html=True
+)
+st.write("#")
+st.image("picture/ilkresim.jpeg", width=700)
+
+if page == "Ana Sayfa":
+    st.markdown(
+        """
+        <style>
+        h1, h2, h3, h4, h5, h6, p {
+            color: white;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    # Başlık
+    st.title("Kaan Danışmanlık")
+
+    st.subheader("Merhabalar ülkemizin değerli misafirleri,")
+    st.write("""
+    Öncelikle hepinizi sevgi ile kucaklıyor, ülkenizden ayrılmanın verdiği hüznü sizlerle paylaşmak istiyoruz.
+    Kaan Danışmanlık olarak uzman kadromuzla birlikte 8 yıldır sizden önceki misafirlerimize kaliteli hizmet sunduk ve bu kalitemizi sizlere de hizmet vererek devam ettirmek istiyoruz.
+    Peki biz 8 yıldır neler mi yapıyoruz?
+    """)
+
+    # Kullanıcı Yorumları
+    st.subheader("Müşteri Yorumları")
+
+    # Yorum 1
+    st.image("picture/peter.jpeg", width=500)  # Yorum sahibinin fotoğrafı
+    st.write("Phillip Mayer, 35")
+    st.write("""
+    "Kaan Danışmanlık ile göçmenlik süreci hakkında endişelenmenize gerek yok. Kendimi hoş ve emin ellerde hissettim."
+    """)
+
+    # Yorum 2
+    st.image("picture/noni.jpeg", width=500)  # Yorum sahibinin fotoğrafı
+    st.write("Noni Faraji, 25")
+    st.write("""
+    "Türkiye'ye tek başıma okumak için gelmek çok stresli olabilirdi. Neyse ki, Kaan Danışmanlık benim evden uzaktaki ailemdi ve her şeyi halletmeme yardım etti."
+    """)
+
 st.write("denem")
 
 st.write("#")
